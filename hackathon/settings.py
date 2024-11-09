ab3f8e46ac383510e3a79f27eb8f5aebf660e5ec
bentoluizv <bentoluizv@gmail.com>
2024-11-09 02:19:21 -0300

    remove unused properties

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -7,8 +7,6 @@ class Settings(BaseSettings):
     )
 
     DATABASE_URL: str
-    SECRET_KEY: str
-    ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     LANGCHAIN_TRACING_V2: bool
     LANGCHAIN_ENDPOINT: str
